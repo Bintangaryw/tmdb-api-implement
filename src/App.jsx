955e55a32eaa9b6da5959bfcd44c05741ae88855
bintang ary <wardhanabintang07@gmail.com>
2024-12-06 18:40:40 +0700

    App.jsx react-router-dom configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3x bg-red-500">Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route path="*" element={<h1>Not Found!</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
