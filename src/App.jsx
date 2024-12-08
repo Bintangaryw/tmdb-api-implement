da0dd6302fe5ce97e4f0a67c3d92a35488f756f8
bintang ary <wardhanabintang07@gmail.com>
2024-12-08 22:32:04 +0700

    Fetching Popular Movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-        <Route path="*" element={<h1>Not Found!</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="*" element={<h1>Not Found!</h1>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+    const [popularMovies, setPopularMovies] = useState([]);
+
+    useEffect(() => {
+        const getPopularMovies = async () => {
+            try {
+                const response = await axios.get(`https://api.themoviedb.org/3/movie/popular`, {
+                    headers: {
+                        Authorization:
+                            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjA5NjYyZDcwNDI5ODFhNThkNThiNjljYTBiZDc0OCIsIm5iZiI6MTY5NTgxODUyOC40NDcsInN1YiI6IjY1MTQyMzIwOWI4NjE2MDBmZjY3OTU1NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gOvyYjmXEmauTFJd8WcEhJTl6gHrayHF8ErMxhozMUk",
+                    },
+                });
+                const f_popularMovie = response.data.results; //mengambil results dari dalam objek data pada response
+                console.log(f_popularMovie);
+                setPopularMovies(f_popularMovie);
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    alert(error?.response?.data?.status_message);
+                    return;
+                }
+                alert(error?.message);
+            }
+        };
+        getPopularMovies();
+    }, []);
+
+    // Display
+    return (
+        <>
+            <div className="">
+                {popularMovies.map((movie) => {
+                    return (
+                        <div className="py-2" key={movie?.id}>
+                            <h2>{movie?.title}</h2>
+                            <p>{movie?.overview}</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+};
+
+export default HomePage;
