324ea20cb644d166d65e5da7902b7311e97e74ba
bintang ary <wardhanabintang07@gmail.com>
2024-12-14 21:06:01 +0700

    Add carousel component

--- src/components/carousel-bleed/CarouselComp.jsx
+++ src/components/carousel-bleed/CarouselComp.jsx
@@ -4,25 +4,21 @@ const CarouselComp = () => {
             <div className="pb-5">
                 <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
                     <div className="carousel-item">
-                        <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" className="rounded-box" />
+                        <div className="relative rounded-box overflow-hidden">
+                            {/* Gradient Overlay */}
+                            <div className="absolute inset-0 bg-gradient-to-t from-slate-950 opacity-100">
+                                <div className="flex flex-col-reverse h-full">
+                                    <p className="text-sm px-2 py-2">10 Dec, 2023</p>
+                                    <p className="text-2xl px-2 font-bold">Marvel Spiderman</p>
+                                </div>
+                            </div>
+
+                            {/* Gambar */}
+                            <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" className="w-full h-full object-cover" />
+                        </div>
                     </div>
                     <div className="carousel-item">
                         <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" className="rounded-box" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" className="rounded-box" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" className="rounded-box" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" className="rounded-box" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" className="rounded-box" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" className="rounded-box" />
                     </div>
                 </div>
             </div>
